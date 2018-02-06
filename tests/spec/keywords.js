1aa126893a03fa998735eead9aa24fe6cba6bc74
Greyarch BV <gantcho@greyarch.net>
2018-02-06 15:32:32 +0100

    added tests for 'run' keyword

--- tests/spec/keywords.js
+++ tests/spec/keywords.js
@@ -1,9 +1,11 @@
 describe("Keyword", () => {
-  it("check should work", () => testx.run("tests/scripts/check.testx"));
-  it("that requires basic navigation should work", () => {
-    testx.run("tests/scripts/cookies.testx");
-    testx.run("tests/scripts/navigate.testx");
+  it("check should check that objects conform", () =>
+    testx.run("tests/scripts/check.testx"));
+  it("that requires basic navigation should work", async () => {
+    await testx.run("tests/scripts/cookies.testx");
+    await testx.run("tests/scripts/navigate.testx");
   });
-  it("wait should work", () =>
+  it("wait should wait for objects to conform", () =>
     testx.run("tests/scripts/wait.testx", { match: "testxio" }));
+  it("run should execute a script", () => testx.run("tests/scripts/run.testx"));
 });
